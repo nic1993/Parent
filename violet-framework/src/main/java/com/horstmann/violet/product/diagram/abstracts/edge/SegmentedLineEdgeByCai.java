937ecb1ed3096b15ed55dc293612a8da0a7f3333
nic1993 <534222412@qq.com>
2016-12-08 19:52:00 +0800

    12-08

--- violet-framework/src/main/java/com/horstmann/violet/product/diagram/abstracts/edge/SegmentedLineEdgeByCai.java
+++ violet-framework/src/main/java/com/horstmann/violet/product/diagram/abstracts/edge/SegmentedLineEdgeByCai.java
@@ -67,7 +67,7 @@ public abstract class SegmentedLineEdgeByCai extends ShapeEdge
     {
         return true;
     }
-    
+     
     @Override
     public void setTransitionPoints(Point2D[] transitionPoints)
     {
